bf01ca10eea464fe7be9c7a73a861684d8109a0b
Lirika12 <uskova-tatyana@mail.ru>
2022-12-20 07:10:25 +0700

    Добавляем карточки котов

---
+++ index.js
@@ -0,0 +1,14 @@
+const $wrapper = document.querySelector('[data-wrapper]');
+const $addButton = document.querySelector('[data-add_button]');
+
+const api = new Api('tilyshchenya')
+
+const gerenationCatCard = (cat) => `<div data-card_id=${cat.id} class="card mx-2" style="width: 18rem;">
+<img src="${cat.image}" class="card-img-top" alt="${cat.name}">
+<div class="card-body">
+  <h5 class="card-title">${cat.name}</h5>
+  <p class="card-text">${cat.description}</p>
+  <button data-action="show" class="btn btn-primary">Show</button>
+  <button data-action="delete" class="btn btn-danger">Delete</button>
+</div>
+</div>`
